8578f642f8265d2d6798f0c135252b3feec97006
Nick Olson <naolson.1819@gmail.com>
2022-09-11 18:00:31 -0500

    created moderation service and event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+const events = [];
+
+app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event); // Posts
-  axios.post('http://localhost:4001/events', event); // Comments
-  axios.post('http://localhost:4002/events', event); // Query
-  axios.post('http://localhost:4003/events', event); // Moderation
+  events.push(event);
 
-  res.send({ status: 'OK' });
+  // Posts
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // Comments
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // Query
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // Moderation
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log('Listening on 4005');
-}
+  console.log("Listening on 4005");
+});
